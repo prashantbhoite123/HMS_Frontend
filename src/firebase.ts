a20d0bd363176b38c3825918241d7a16d252ed2b
prashantbhoite123 <pbhoite985@gmail.com>
2024-09-28 18:04:22 +0530

    add firebase in client

---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// import { } from "firebase/a"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBX_H0pWGAZU2lAminfvaYKK35VbSZvfW0",
+  authDomain: "hospital-cda26.firebaseapp.com",
+  projectId: "hospital-cda26",
+  storageBucket: "hospital-cda26.appspot.com",
+  messagingSenderId: "1009659914363",
+  appId: "1:1009659914363:web:8705bb3713bf0cd81a5ade",
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
