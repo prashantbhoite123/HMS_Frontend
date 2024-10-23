55de17af228e27c2dcefbbade40a953293558036
prashantbhoite123 <pbhoite985@gmail.com>
2024-10-23 09:02:57 +0530

    complete appoinment form

--- src/form/Common_Form/FormInput.tsx
+++ src/form/Common_Form/FormInput.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-
 import {
   FormField,
   FormItem,
@@ -13,10 +12,10 @@ interface FormInputProps {
   name: string
   label: string
   placeholder: string
-  type?: string
-  defaultValue?: any
-  min?: number | undefined
-  max?: number | undefined
+  type?: "text" | "date" // Restrict the type to "text" and "date"
+  defaultValue?: string // Use string for defaultValue, especially for date inputs
+  min?: string // For date inputs, min should be a string
+  max?: string // For date inputs, max should be a string
 }
 
 const FormInput: React.FC<FormInputProps> = ({
@@ -33,12 +32,12 @@ const FormInput: React.FC<FormInputProps> = ({
       name={name}
       render={({ field }) => (
         <FormItem>
-          <FormLabel className="font-semibold ">{label}</FormLabel>
+          <FormLabel className="font-semibold">{label}</FormLabel>
           <FormMessage className="text-red-600" />
           <FormControl>
             <Input
               placeholder={placeholder}
-              className="text-black  rounded-[5px] border border-gray-300 placeholder:text-slate-700 px-4 focus:outline-cyan-600 "
+              className="text-black  rounded-[5px] border border-gray-300 placeholder:text-slate-700 px-4 focus:outline-cyan-600"
               autoFocus
               defaultValue={defaultValue}
               type={type}
--- src/form/Patient/Appoinment.tsx
+++ src/form/Patient/Appoinment.tsx
@@ -1,3 +1,144 @@
+// import { zodResolver } from "@hookform/resolvers/zod"
+// import { Controller, FormProvider, useForm } from "react-hook-form"
+// import { z } from "zod"
+// import FormInput from "../Common_Form/FormInput"
+// import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer"
+// import { Button } from "@/components/ui/button"
+// import {
+//   FormControl,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form"
+// import { Textarea } from "@/components/ui/textarea"
+
+// const formData = z.object({
+//   patientName: z.string().trim().min(1, { message: "patientName is required" }),
+//   doctorName: z.string().trim().min(1, { message: "doctorName is required" }),
+//   appointmentDate: z.date({
+//     required_error: "Appointment date is required",
+//     invalid_type_error: "Invalid date format",
+//   }),
+//   reason: z.string().trim().min(1, { message: "reason is required" }),
+// })
+
+// export type appoinment = z.infer<typeof formData>
+
+// const onSubmit = (data: appoinment) => {
+//   console.log(data)
+// }
+
+// export interface IDoctors extends Document {
+//   doctorName: string
+//   education: string
+//   experienceYears: number
+//   specialization: string
+//   workingHours: string
+// }
+
+// type Props = {
+//   doctors: IDoctors[]
+// }
+// const Appoinment = ({ doctors }: Props) => {
+//   const form = useForm<appoinment>({
+//     resolver: zodResolver(formData),
+//   })
+//   const { control } = form
+//   return (
+//     <>
+//       <Drawer>
+//         <DrawerTrigger>
+//           <div className="">
+//             <Button
+//               type="button"
+//               variant="outline"
+//               className="w-full bg-green-500 text-[1.1.4rem] shadow-sm text-white"
+//             >
+//               Book Appoinment
+//             </Button>
+//           </div>
+//         </DrawerTrigger>
+//         <DrawerContent className="h-screen">
+//           <FormProvider {...form}>
+//             <form onSubmit={form.handleSubmit(onSubmit)}>
+//               <div className="w-full h-full flex justify-center items-center">
+//                 <div className="flex flex-col items-center mt-7 p-6 w-full md:w-[40vw] bg-white shadow-2xl">
+//                   <h1 className="text-lg text-green-400 font-semibold">
+//                     Book Appoinment
+//                   </h1>
+//                   <div className="flex flex-col w-full gap-5 mt-5 justify-center">
+//                     <FormInput
+//                       name="patientName"
+//                       type="text"
+//                       label="Patient Name"
+//                       placeholder="patientname"
+//                     />
+//                     <label
+//                       htmlFor="doctorName"
+//                       className="text-sm font-semibold"
+//                     >
+//                       Doctor Name
+//                     </label>
+//                     <Controller
+//                       name="doctorName"
+//                       control={control}
+//                       render={({ field }) => (
+//                         <select
+//                           {...field}
+//                           className="block w-full cursor-pointer appearance-none text-black bg-white border border-gray-300 rounded-md py-2 px-3 pr-10 text-base focus:outline-none focus:ring-indigo-500 focus:border-green-400 sm:text-sm"
+//                         >
+//                           {doctors.map((doctor: IDoctors) => (
+//                             <option
+//                               key={doctor.doctorName}
+//                               value={doctor.doctorName}
+//                             >
+//                               {doctor.doctorName}
+//                             </option>
+//                           ))}
+//                         </select>
+//                       )}
+//                     />
+//                     <FormInput
+//                       name="appointmentDate"
+//                       type="date"
+//                       label="AppointmentDate"
+//                       placeholder="appointmentDate"
+//                     />
+//                     <div className="w-full ">
+//                       <Controller
+//                         name="reason"
+//                         rules={{ required: "Description is required" }}
+//                         render={({ field }) => (
+//                           <FormItem>
+//                             <FormLabel>Reason</FormLabel>
+//                             <FormControl>
+//                               <Textarea
+//                                 {...field}
+//                                 className="resize-none  border border-gray-300 rounded-[5px] text-black focus:outline-cyan-600"
+//                                 autoFocus
+//                                 placeholder="Add reason"
+//                               />
+//                             </FormControl>
+
+//                             <FormMessage className="text-red-600"></FormMessage>
+//                           </FormItem>
+//                         )}
+//                       />
+//                     </div>
+//                     <Button className="bg-green-400 text-lg ">Submit</Button>
+//                   </div>
+//                 </div>
+//               </div>
+//             </form>
+//           </FormProvider>
+//         </DrawerContent>
+//       </Drawer>
+//     </>
+//   )
+// }
+
+// export default Appoinment
+
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Controller, FormProvider, useForm } from "react-hook-form"
 import { z } from "zod"
@@ -13,24 +154,25 @@ import {
 import { Textarea } from "@/components/ui/textarea"
 
 const formData = z.object({
-  patientName: z.string().trim().min(1, { message: "patientName is required" }),
-  doctorName: z.string().trim().min(1, { message: "doctorName is required" }),
-  appointmentDate: z
+  patientName: z
     .string()
     .trim()
-    .refine((value) => !isNaN(Date.parse(value)), {
-      message: "appointmentDate is must be formated",
-    }),
-  reason: z.string().trim().min(1, { message: "reason is required" }),
+    .min(1, { message: "Patient name is required" }),
+  doctorName: z.string().trim().min(1, { message: "Doctor name is required" }),
+  appointmentDate: z.date({
+    required_error: "Appointment date is required",
+    invalid_type_error: "Invalid date format",
+  }),
+  reason: z.string().trim().min(1, { message: "Reason is required" }),
 })
 
-export type appoinment = z.infer<typeof formData>
+export type Appointment = z.infer<typeof formData>
 
-const onSubmit = (data: appoinment) => {
+const onSubmit = (data: Appointment) => {
   console.log(data)
 }
 
-export interface IDoctors extends Document {
+export interface IDoctors {
   doctorName: string
   education: string
   experienceYears: number
@@ -41,11 +183,16 @@ export interface IDoctors extends Document {
 type Props = {
   doctors: IDoctors[]
 }
+
 const Appoinment = ({ doctors }: Props) => {
-  const form = useForm<appoinment>({
+  const form = useForm<Appointment>({
     resolver: zodResolver(formData),
+    defaultValues: {
+      appointmentDate: new Date(), // Default today's date
+    },
   })
   const { control } = form
+
   return (
     <>
       <Drawer>
@@ -54,9 +201,9 @@ const Appoinment = ({ doctors }: Props) => {
             <Button
               type="button"
               variant="outline"
-              className="w-full bg-green-500 text-[1.1.4rem] shadow-sm text-white"
+              className="w-full bg-green-500 text-[1.1rem] shadow-sm text-white"
             >
-              Book Appoinment
+              Book Appointment
             </Button>
           </div>
         </DrawerTrigger>
@@ -64,17 +211,18 @@ const Appoinment = ({ doctors }: Props) => {
           <FormProvider {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}>
               <div className="w-full h-full flex justify-center items-center">
-                <div className="flex flex-col items-center mt-7 p-6 w-full md:w-[50vw] bg-white shadow-2xl">
+                <div className="flex flex-col items-center mt-7 p-6 w-full md:w-[40vw] bg-white shadow-2xl">
                   <h1 className="text-lg text-green-400 font-semibold">
-                    Book Appoinment
+                    Book Appointment
                   </h1>
                   <div className="flex flex-col w-full gap-5 mt-5 justify-center">
                     <FormInput
                       name="patientName"
                       type="text"
                       label="Patient Name"
-                      placeholder="patientname"
+                      placeholder="Enter patient name"
                     />
+
                     <label
                       htmlFor="doctorName"
                       className="text-sm font-semibold"
@@ -89,6 +237,9 @@ const Appoinment = ({ doctors }: Props) => {
                           {...field}
                           className="block w-full cursor-pointer appearance-none text-black bg-white border border-gray-300 rounded-md py-2 px-3 pr-10 text-base focus:outline-none focus:ring-indigo-500 focus:border-green-400 sm:text-sm"
                         >
+                          <option value="" disabled>
+                            Select a doctor
+                          </option>
                           {doctors.map((doctor: IDoctors) => (
                             <option
                               key={doctor.doctorName}
@@ -100,34 +251,36 @@ const Appoinment = ({ doctors }: Props) => {
                         </select>
                       )}
                     />
+
                     <FormInput
                       name="appointmentDate"
                       type="date"
-                      label="AppointmentDate"
-                      placeholder="appointmentDate"
+                      label="Appointment Date"
+                      defaultValue={new Date().toISOString().split("T")[0]} // Sets today's date in "YYYY-MM-DD"
+                      placeholder="" // Can be removed, not visible in most browsers for date input
                     />
+
                     <div className="w-full ">
                       <Controller
                         name="reason"
-                        rules={{ required: "Description is required" }}
+                        rules={{ required: "Reason is required" }}
                         render={({ field }) => (
                           <FormItem>
                             <FormLabel>Reason</FormLabel>
                             <FormControl>
                               <Textarea
                                 {...field}
-                                className="resize-none  border border-gray-300 rounded-[5px] text-black focus:outline-cyan-600"
-                                autoFocus
+                                className="resize-none border border-gray-300 rounded-[5px] text-black focus:outline-cyan-600"
                                 placeholder="Add reason"
                               />
                             </FormControl>
-
                             <FormMessage className="text-red-600"></FormMessage>
                           </FormItem>
                         )}
                       />
                     </div>
-                    <Button className="bg-green-400 text-lg ">Submit</Button>
+
+                    <Button className="bg-green-400 text-lg">Submit</Button>
                   </div>
                 </div>
               </div>
