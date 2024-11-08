5b2bba544dbfc4c839ff307b28fb9c17735b79ef
prashantbhoite123 <pbhoite985@gmail.com>
2024-11-08 23:30:36 +0530

    set dash default route

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -46,7 +46,7 @@ const Header = () => {
             </Button>
           </Link>
         ) : (
-          <Link to="/dashboard">
+          <Link to="/dashboard?tab=dash">
             <Button className="flex items-center gap-2 text-lg font-semibold bg-gradient-to-r from-red-500 to-pink-400 text-white px-6 py-2 rounded-md transition-transform duration-300 hover:scale-105 hover:underline shadow-md">
               <Hospital className="text-xl" />
               <span>Dashboard</span>
