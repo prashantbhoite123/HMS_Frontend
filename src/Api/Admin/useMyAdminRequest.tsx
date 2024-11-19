287cee4d5bae41a0eaeb75d7e4961196e7a008b5
prashantbhoite123 <pbhoite985@gmail.com>
2024-11-19 16:30:01 +0530

    create frontend side rejection api

--- src/Api/Admin/useMyAdminRequest.tsx
+++ src/Api/Admin/useMyAdminRequest.tsx
@@ -1,5 +1,6 @@
 import { BACKEND_API_URL } from "@/main"
-import { useQuery } from "react-query"
+import { useMutation, useQuery } from "react-query"
+import { toast } from "sonner"
 
 export const useMyRequestedHospital = () => {
   const getMyRequestHos = async () => {
@@ -35,4 +36,41 @@ export const useMyRequestedHospital = () => {
     }
   )
   return { requestedHospital, isLoading }
+}
+
+export const useMyRejectHospital = (hospitalId: string) => {
+  const hospitalRejectApi = async (reson: string) => {
+    const responce = await fetch(
+      `${BACKEND_API_URL}/api/approvel/rejected/${hospitalId as string}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(reson),
+      }
+    )
+    if (!responce.ok) {
+      throw new Error("failed to Reject user")
+    }
+
+    const data = await responce.json()
+    if (data.status === false) {
+      return toast.error(data.message)
+    }
+
+    toast.success(data.message)
+    return data
+  }
+
+  const { mutate: rejectionHos, isLoading } = useMutation(hospitalRejectApi, {
+    onSuccess: () => {
+      console.log("hospital rejected successful")
+    },
+    onError: () => {
+      console.log("error to hospital rejection ")
+    },
+  })
+
+  return { rejectionHos, isLoading }
 }
